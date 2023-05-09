fa23154c7a62584364f83d1136ae719e3cd0efe0
cet <67916285+cet-t@users.noreply.github.com>
2023-05-09 23:10:18 +0900

    add: feature set files

---
+++ SceneLoading.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+namespace GameTitle
+{
+    public class SceneLoading : MonoBehaviour
+    {
+        [SerializeField]
+        GameObject panel;
+        [SerializeField]
+        Slider slider;
+        [SerializeField]
+        Button button;
+
+        public void LoadingNext()
+        {
+            panel.SetActive(true);
+            StartCoroutine(LoadingScene());
+        }
+
+        IEnumerator LoadingScene()
+        {
+            var async = SceneManager.LoadSceneAsync(Scenes.Main);
+
+            while (!async.isDone)
+            {
+                slider.value = async.progress;
+                yield return null;
+            }
+        }
+    }
+}
